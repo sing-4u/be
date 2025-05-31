be0820532dc5603ac73f74b8ee96881d9f5d6858
yongcrane96 <dydgkr3363@naver.com>
2025-05-31 22:23:07 +0900

    feat-스포티 파이 연동 및 @ManyToOne 관계 로직 제거
    feat-스포티 파이 연동 및 @ManyToOne 관계 로직 제거

--- src/main/java/com/sing4u/kr/customSongRequest/entity/SongRequest.java
+++ src/main/java/com/sing4u/kr/customSongRequest/entity/SongRequest.java
@@ -20,9 +20,8 @@ public class SongRequest {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // songRequestId
 
-    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "session_id", nullable = false)
-    private Session session;
+    private Long sessionId;
 
     @Column(name = "fan_email", length = 50)
     private String fanEmail;
@@ -41,15 +40,6 @@ public class SongRequest {
     @Column(name = "requested_at", nullable = false, updatable = false)
     private LocalDateTime requestedAt;
 
-    public static SongRequest fromCreateDto(Session session, SongRequestCreateDto dto) {
-        return SongRequest.builder()
-                .session(session)
-                .fanEmail(dto.getEmail())
-                .songTitle(dto.getSongTitle())
-                .songArtistName(dto.getArtistName())
-                .spotifyTrackId(dto.getSpotifyTrackId())
-                .build();
-    }
 }
 
 
