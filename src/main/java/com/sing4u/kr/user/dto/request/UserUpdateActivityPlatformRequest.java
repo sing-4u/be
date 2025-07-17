32201421bb261d84afb4d1127783331bc686c850
YeonjeKo <kyounje@naver.com>
2025-07-17 17:25:22 +0900

    Update UserUpdateActivityPlatformRequest.java
    활동플랫폼수정api 스웨거에 대괄호 추가

--- src/main/java/com/sing4u/kr/user/dto/request/UserUpdateActivityPlatformRequest.java
+++ src/main/java/com/sing4u/kr/user/dto/request/UserUpdateActivityPlatformRequest.java
@@ -15,7 +15,8 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserUpdateActivityPlatformRequest {
 
-    @Schema(description = "수정할 활동 플랫폼 목록", implementation = ActivityPlatformRequest.class)
+    @Schema(description = "수정할 활동 플랫폼 목록", implementation = ActivityPlatformRequest.class,
+            example = "[{\"platformType\": \"YOUTUBE\", \"platformUrl\": \"https://www.youtube.com/channel/UC-abcdefg\"}]")
     @NotEmpty(message = "활동 플랫폼 리스트를 입력해주세요.")
     @Size(min = 1, message = "최소 1개 이상의 활동 플랫폼이 필요합니다.")
     private List<ActivityPlatformRequest> activityPlatforms;
