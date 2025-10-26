95158cad3ac5293e04d03fba7e5f425c57f34379
yongcrane96 <dydgkr3363@naver.com>
2025-10-26 18:35:55 +0900

    곡추천_태그,URL 추가로 인한 테스트 코드 작성
    곡추천_태그,URL 추가로 인한 테스트 코드 작성

--- src/main/java/com/sing4u/kr/songRequest/entity/SongRequest.java
+++ src/main/java/com/sing4u/kr/songRequest/entity/SongRequest.java
@@ -6,6 +6,7 @@ import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,6 +39,14 @@ public class SongRequest {
 
     @Column(name = "song_artist_name", length = 100)
     private String songArtistName; // 노래의 아티스트명
+
+    @ElementCollection
+    @CollectionTable(name = "song_request_tags", joinColumns = @JoinColumn(name = "song_request_id"))
+    @Column(name = "tag", length = 20)
+    private List<String> tags;
+
+    @Column(name = "url", length = 255)
+    private String url;
 
     @CreationTimestamp
     @Column(name = "requested_at", nullable = false, updatable = false, columnDefinition = "datetime")
