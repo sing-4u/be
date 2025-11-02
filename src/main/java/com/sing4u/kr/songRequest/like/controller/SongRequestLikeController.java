44be5037557f71957e122f20c73ebac833626873
YeonjeKo <kyounje@naver.com>
2025-11-02 21:30:43 +0900

    refactor: rename parameter
    requestId -> songRequestId

--- src/main/java/com/sing4u/kr/songRequest/like/controller/SongRequestLikeController.java
+++ src/main/java/com/sing4u/kr/songRequest/like/controller/SongRequestLikeController.java
@@ -16,7 +16,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/song-requests/{requestId}/likes")
+@RequestMapping("/api/v1/song-requests/{songRequestId}/likes")
 @RequiredArgsConstructor
 public class SongRequestLikeController {
     private final SongRequestLikeService service;
